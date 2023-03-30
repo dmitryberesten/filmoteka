99dcc30acbb85d0b6940c88739005f542c4ab0c7
Dima <dmitryberesten@gmail.com>
2023-03-31 00:22:41 +0300

    add render

--- src/js/create-gallery-markup.js
+++ src/js/create-gallery-markup.js
@@ -1,35 +1,3 @@
 // РЕКОМЕНДАЦІЇ
 // 1. Створити та експортувати функцію яка перебирає фільми та повертає розмітку.
-
-import { fetchPopularMovies } from './api';
-
-function renderMovies(data) {
-  const moviesEl = document.querySelector('.movies');
-  data.results.forEach(movie => {
-    const movieEl = document.createElement('li');
-    movieEl.classList.add('movie');
-    movieEl.dataset.id = movie.id;
-    movieEl.innerHTML = `
-    <div class="movie__cover-inner">
-        <img
-          src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
-          class="movie__cover"
-          alt="${movie.title}"
-        />
-        <div class="movie__cover--darkened"></div>
-    </div>
-      <div class="movie__info">
-        <div class="movie__title">${movie.title}</div>
-        <div class="movie__genre">${movie.genre_ids.map(
-          genre => ` ${genre}`
-        )}</div>
-
-        <div class="movie__average">${movie.vote_average.toFixed(1)}</div>
-    
-      </div>`;
-    moviesEl.appendChild(movieEl);
-  });
-}
-fetchPopularMovies(1).then(res => {
-  renderMovies(res);
-});
+// ТУТ ПРАЦЮЄ ОЛЕКСАНДР ХИМЕНКО
--- src/js/rendering-movie-cards.js
+++ src/js/rendering-movie-cards.js
@@ -3,4 +3,36 @@
 // 2. Створити розмітку для галереї фільмів.
 // 3. Додати пагінацію на основі загальної кількості результатів та загальної кількості сторінок.
 
-// test Khymenko
+
+import { fetchPopularMovies } from './api';
+
+function renderMovies(data) {
+  const moviesEl = document.querySelector('.movies');
+  data.results.forEach(movie => {
+    const movieEl = document.createElement('li');
+    movieEl.classList.add('movie');
+    movieEl.dataset.id = movie.id;
+    movieEl.innerHTML = `
+    <div class="movie__cover-inner">
+        <img
+          src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+          class="movie__cover"
+          alt="${movie.title}"
+        />
+        <div class="movie__cover--darkened"></div>
+    </div>
+      <div class="movie__info">
+        <div class="movie__title">${movie.title}</div>
+        <div class="movie__genre">${movie.genre_ids.map(
+          genre => ` ${genre}`
+        )}</div>
+
+        <div class="movie__average">${movie.vote_average.toFixed(1)}</div>
+
+      </div>`;
+    moviesEl.appendChild(movieEl);
+  });
+}
+fetchPopularMovies(1).then(res => {
+  renderMovies(res);
+});
