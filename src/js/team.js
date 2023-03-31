63506deb128da56511821aeecf6cb3a64c9cee27
YuliyaBondG <manyunka@ukr.net>
2023-03-31 10:00:02 +0600

    correct footer

--- src/js/team.js
+++ src/js/team.js
@@ -1,2 +1,107 @@
-// РЕКОМЕНДАЦІЇ
-// 1. Створити масив об'єктів з учасниками команди та експортвати.
+import photoBeresten from "../images/team/Beresten.jpeg";
+import photoMyhulia from "../images/team/Myhulia.jpeg";
+// import photo from "../images/team/";  Inna
+// import photo from "../images/team/";  Oleksandr Mix
+// import photo from "../images/team/";  Oleksandr H
+import photoBondarenkoI from "../images/team/BondarenkoI.jpeg";
+// import photo from "../images/team/";  Oleksandr
+// import photo from "../images/team/"; Yuliya
+import photoBondarenkoY from "../images/team/BondarenkoY.jpeg";
+// import photo from "../images/team/"; Dlada
+// import photo from "../images/team/"; Yana x
+// import photo from "../images/team/";  Muxailo
+// import photo from "../images/team/";  Svetlana
+// import photo from "../images/team/";  Bogdan
+
+
+
+export const teamList = [
+    {
+        name: 'Dmytrii Beresten',
+        role: 'Team Lead',
+        github: 'https://github.com/dmitryberesten',
+        photo: photoBeresten,
+    },
+    {
+        name: 'Maksym Myhulia',
+        role: 'Scrum Master',
+        github: 'https://github.com/MaksymMyhulia',
+        photo: photoMyhulia,
+    },
+    {
+        name: 'Inna',
+        role: 'Developer',
+        github: '',
+        photo: '',
+    },
+    {
+        name: 'Oleksandr',
+        role: 'Developer',
+        github: '',
+        photo: '',
+    },
+    {
+        name: 'Oleksandr H',
+        role: 'Developer',
+        github: '',
+        photo: '',
+    },
+    {
+        name: 'Iryna Bondarenko',
+        role: 'Developer',
+        github: 'https://github.com/IrynaBondarenko7 ',
+        photo: photoBondarenkoI,
+    },
+    {
+        name: 'Oleksandr H',
+        role: 'Developer',
+        github: '',
+        photo: '',
+    },
+    {
+        name: 'Yuliya',
+        role: 'Developer',
+        github: '',
+        photo: '',
+    },
+    {
+        name: 'Yuliya Bondarenko',
+        role: 'Developer',
+        github: 'https://github.com/YuliyaBondG',
+        photo: photoBondarenkoY,
+    },
+    {
+        name: 'Dlada',
+        role: 'Developer',
+        github: '',
+        photo: '',
+    },
+    {
+        name: 'Yana',
+        role: 'Developer',
+        github: '',
+        photo: '',
+    },
+    {
+        name: 'Myxailo',
+        role: 'Developer',
+        github: '',
+        photo: '',
+    },
+    {
+        name: 'Svetlana',
+        role: 'Developer',
+        github: '',
+        photo: '',
+    },
+    {
+        name: 'Bogdan',
+        role: 'Developer',
+        github: '',
+        photo: '',
+    },
+
+
+
+
+]
