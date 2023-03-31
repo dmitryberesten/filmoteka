d35192aa1e7a58f37ec83259cd68d955c3307ed4
Universe2022 <svitlankagarna05@gmail.com>
2023-03-31 12:45:12 +0300

    Update loader.js

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,2 +1,24 @@
 // РЕКОМЕНДАЦІЇ
 // 1. Додати логіку показу/скриття лоадера.
+
+const sk = {
+  html: `<div class="sk-chase sk-center">
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+        </div>`,
+
+  run(tag) {
+    //...
+
+    tag.insertAdjacentHTML('beforeend', this.html);
+  },
+
+  stop() {
+    const divSpinner = document.querySelector('div.sk-chase');
+    divSpinner.parentNode.removeChild(divSpinner);
+  },
+};
