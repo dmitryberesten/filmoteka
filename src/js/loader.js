87680fc4225d46cc89c87c1bee3c1cbdc7e897d7
Universe2022 <svitlankagarna05@gmail.com>
2023-04-01 15:27:33 +0300

    Adding preloader & loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,24 +1,51 @@
 // РЕКОМЕНДАЦІЇ
 // 1. Додати логіку показу/скриття лоадера.
 
-const sk = {
-  html: `<div class="sk-chase sk-center">
-            <div class="sk-chase-dot"></div>
-            <div class="sk-chase-dot"></div>
-            <div class="sk-chase-dot"></div>
-            <div class="sk-chase-dot"></div>
-            <div class="sk-chase-dot"></div>
-            <div class="sk-chase-dot"></div>
-        </div>`,
+// Preloader //
 
-  run(tag) {
-    //...
+const preloaderEl = document.querySelector('.preloader');
 
-    tag.insertAdjacentHTML('beforeend', this.html);
-  },
+if (preloaderEl) {
+  window.addEventListener('load', hidePreloader);
 
-  stop() {
-    const divSpinner = document.querySelector('div.sk-chase');
-    divSpinner.parentNode.removeChild(divSpinner);
-  },
-};
+  function hidePreloader() {
+    document.body.style.overflow = 'hidden';
+
+    setTimeout(() => {
+      preloaderEl.classList.add('preloader--hide');
+      document.body.style.overflow = 'auto';
+    }, 2500);
+    setTimeout(() => {
+      preloaderEl.remove();
+    }, 3500);
+  }
+}
+// Loader-spinner //
+
+function showLoader() {
+  document.body.style.overflow = 'hidden';
+
+  const loaderSpinner = `
+    <div class="loading">
+      <div class="loading__spinner"></div>
+      <img class="loading__logo" src="${new URL(
+        '../images/logo.png',
+        import.meta.url
+      )}" alt="logo" />
+      <p class="loading__text">Loading...</p>
+    </div>`;
+
+  document.body.insertAdjacentHTML('afterbegin', loaderSpinner);
+}
+
+function hideLoader() {
+  const loaderSpinnerEl = document.querySelector('.loading');
+  loaderSpinnerEl.classList.add('loading--hide');
+
+  setTimeout(() => {
+    loaderSpinnerEl.remove();
+    document.body.style.overflow = 'auto';
+  }, 300);
+}
+
+export { showLoader, hideLoader };
