4945f3cd5a6fe9777151670dbcbc755fb37c3fb1
Maksym Myhulia <113797631+MaksymMyhulia@users.noreply.github.com>
2023-04-01 11:34:17 +0300

    add_custom-anchor

--- src/js/custom-components.js
+++ src/js/custom-components.js
@@ -10,3 +10,31 @@ function myFunction() {
 }
 
 //-----------------------------HORYZONTAL-INDICATOR-----------------------------//
+
+//--------------------------------CUSTOM-ANCHOR---------------------------------//
+
+//Make sure the user has scrolled at least double the height of the browser
+var toggleHeight = $(window).outerHeight() * 1.2;
+
+$(window).scroll(function () {
+    if ($(document).scrollTop() > toggleHeight) {
+        //Adds active class to make button visible
+        $("#up-button").addClass("active");
+
+    } else {
+        //Removes active class to make button visible
+        $("#up-button").removeClass("active");
+    }
+});
+const circleUp = document.querySelector(".up-button");
+
+circleUp.addEventListener("click", setScrollToUp);
+
+function setScrollToUp() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+};
+
+//------------------------------CUSTOM-ANCHOR-END-------------------------------//
