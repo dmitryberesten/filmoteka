ecb75a625a40d7e1cdcf287555c7b82c483b789e
Universe2022 <svitlankagarna05@gmail.com>
2023-04-01 17:53:34 +0300

    Update loader.js

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,51 +1,8 @@
 // РЕКОМЕНДАЦІЇ
 // 1. Додати логіку показу/скриття лоадера.
 
-// Preloader //
+import { refs } from './constants/refs';
 
-const preloaderEl = document.querySelector('.preloader');
-
-if (preloaderEl) {
-  window.addEventListener('load', hidePreloader);
-
-  function hidePreloader() {
-    document.body.style.overflow = 'hidden';
-
-    setTimeout(() => {
-      preloaderEl.classList.add('preloader--hide');
-      document.body.style.overflow = 'auto';
-    }, 2500);
-    setTimeout(() => {
-      preloaderEl.remove();
-    }, 3500);
-  }
+export default function loader() {
+  refs.loader.classList.toggle('backdrop__is-hidden');
 }
-// Loader-spinner //
-
-function showLoader() {
-  document.body.style.overflow = 'hidden';
-
-  const loaderSpinner = `
-    <div class="loading">
-      <div class="loading__spinner"></div>
-      <img class="loading__logo" src="${new URL(
-        '../images/logo.png',
-        import.meta.url
-      )}" alt="logo" />
-      <p class="loading__text">Loading...</p>
-    </div>`;
-
-  document.body.insertAdjacentHTML('afterbegin', loaderSpinner);
-}
-
-function hideLoader() {
-  const loaderSpinnerEl = document.querySelector('.loading');
-  loaderSpinnerEl.classList.add('loading--hide');
-
-  setTimeout(() => {
-    loaderSpinnerEl.remove();
-    document.body.style.overflow = 'auto';
-  }, 300);
-}
-
-export { showLoader, hideLoader };
