11dc444251b113e86525824ed7305b2664e979bb
Dima <dmitryberesten@gmail.com>
2023-04-02 20:08:54 +0300

    add loader fix

--- src/library.js
+++ src/library.js
@@ -5,6 +5,7 @@
 import { page } from './js/api';
 import './js/api';
 import './js/trailer';
+import './js/loader';
 import * as renderingFromStorage from './js/render-from-storage';
 import * as openModal from './js/open-and-close-modal';
 import { refs } from './js/refs';
