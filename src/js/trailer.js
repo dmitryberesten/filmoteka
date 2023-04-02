b8b2a66729a2e28efcb25324d63274771675587e
VladyslavaDvorovenko <chempion0308@gmail.com>
2023-04-02 14:32:01 +0300

    converted trailer

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,36 +1,50 @@
 import * as basicLightbox from 'basiclightbox';
-import { fetchTrailerById } from './api';
+import { API_KEY } from './api';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
-export const openTrailer = async evt => {
-  evt.preventDefault();
-  if (evt.target.nodeName !== 'A') return;
-  try {
-    const { results } = await fetchTrailerById(evt.target.dataset.id);
-    const { key } = results[0];
-    const closeModal = e => {
-      if (e.code === 'Escape') {
-        instance.close();
-      }
-    };
-    const instance = basicLightbox.create(
-      `<iframe class="youtube-frame" width="900" height="600" src="https://www.youtube.com/embed/${key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<button class="close-modal__trailer">     
-     </button>`,
-      {
-        onShow: () => {
-          document.addEventListener('keydown', closeModal);
-        },
-        onClose: () => {
-          document.removeEventListener('keydown', closeModal);
-        },
-      }
-    );
+const srcTrailer = 'https://www.youtube.com/embed/';
 
-    instance.show();
-  } catch (error) {
-    console.error(error.message);
-  }
-};
+const modal = basicLightbox.create(`
+  <iframe width="900" height="600" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<button class="close-modal__trailer">     
+     </button>`);
+const iframeTrailer = modal.element().querySelector('iframe');
+
+const modal2 = basicLightbox.create(`
+    <iframe width="900" height="600" src="${srcTrailer}6DhiiFGk4_s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      `);
+
+export default function openTrailer(id) {
+  findTrailer(id)
+    .then(data => {
+      const key = data.results[0].key;
+
+      iframeTrailer.src = srcTrailer + key;
+      modal.show();
+
+      const closeBtn = document.querySelector('.close-modal__trailer');
+      closeBtn.addEventListener('click', closeModal);
+      const closeModal = e => {
+        if (e.code === 'Escape') {
+          instance.close();
+        }
+        window.removeEventListener('keydown', closeModalHandler);
+      };
+    })
+    .catch(error => {
+      //   const modal = basicLightbox.create(`
+      // <iframe width="860" height="615" src="${srcTrailer}6DhiiFGk4_s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      //   `);
+      modal2.show();
+      console.log(error);
+    });
+}
+
+async function findTrailer(idCard) {
+  const respons = await fetch(
+    `https://api.themoviedb.org/3/movie/${idCard}/videos?api_key=${API_KEY}&language=en-US`
+  );
+  return await respons.json();
+}
 const trailerBtn = document.querySelector('.trailer-btn');
 trailerBtn.addEventListener('click', openTrailer);
