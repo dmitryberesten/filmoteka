e64fc9feeaea99eb43d36002636de8919b6becc2
Dima <dmitryberesten@gmail.com>
2023-04-03 20:19:39 +0300

    add modal-team to library

--- src/library.js
+++ src/library.js
@@ -5,7 +5,7 @@ import * as customComponent from './js/custom-components';
 import './js/trailer';
 import './js/loader';
 import * as renderingFromStorage from './js/render-from-storage';
-import * as openModal from './js/open-and-close-modal';
+import * as teamModal from './js/team-load-to-modal';
 import { refs } from './js/refs';
 import { onBtnAddToLibrary } from './js/add-to-watched&queue';
 window.addEventListener('load', renderingFromStorage.loadFromStorageWatched);
