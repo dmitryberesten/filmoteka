b3eee236ffb15f0ccc6563a6aeb49cced02c9f6c
Dima <dmitryberesten@gmail.com>
2023-04-03 20:30:59 +0300

    Update library.js

--- src/library.js
+++ src/library.js
@@ -1,12 +1,13 @@
 import { page } from './js/api';
 import './js/api';
 import './js/sound';
-import * as customComponent from './js/custom-components';
+import './js/custom-components';
 import './js/trailer';
 import './js/loader';
+
 import * as renderingFromStorage from './js/render-from-storage';
-import * as openModal from './js/open-and-close-modal';
-// import * as teamModal from './js/team-load-to-modal';
+import './js/team-load-to-modal';
+import './js/open-and-close-modal';
 import { refs } from './js/refs';
 import { onBtnAddToLibrary } from './js/add-to-watched&queue';
 window.addEventListener('load', renderingFromStorage.loadFromStorageWatched);
