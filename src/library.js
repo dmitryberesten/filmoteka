d8ed00868b06b3de3932ed399ed0006b1de3259e
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-04-03 12:09:55 +0100

    added update markup after remove films from library

--- src/library.js
+++ src/library.js
@@ -5,8 +5,10 @@ import './js/loader';
 import * as renderingFromStorage from './js/render-from-storage';
 import * as openModal from './js/open-and-close-modal';
 import { refs } from './js/refs';
+import { onBtnAddToLibrary } from './js/add-to-watched&queue';
 window.addEventListener('load', renderingFromStorage.loadFromStorageWatched);
 refs.watchedQueueBtnBlock.addEventListener(
   'click',
   renderingFromStorage.onClickWatched
 );
+refs.btnWrapper.addEventListener('click', onBtnAddToLibrary);
